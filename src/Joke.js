1057d042fbffdabadd28e634ef11594de73f02e9
Daniel Bae <danibae91@icloud.com>
2021-09-20 13:39:04 -0700

    refactor code to Class Component

--- src/Joke.js
+++ src/Joke.js
@@ -1,27 +1,71 @@
 import React from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
+// Functional Component
+// function Joke({ vote, votes, text, id }) {
+//   const upVote = () => vote(id, +1);
+//   const downVote = () => vote(id, -1);
 
-  return (
-    <div className="Joke">
-      <div className="Joke-votearea">
-        <button onClick={upVote}>
-          <i className="fas fa-thumbs-up" />
-        </button>
+//   return (
+//     <div className="Joke">
+//       <div className="Joke-votearea">
+//         <button onClick={upVote}>
+//           <i className="fas fa-thumbs-up" />
+//         </button>
 
-        <button onClick={downVote}>
-          <i className="fas fa-thumbs-down" />
-        </button>
+//         <button onClick={downVote}>
+//           <i className="fas fa-thumbs-down" />
+//         </button>
 
-        {votes}
+//         {votes}
+//       </div>
+
+//       <div className="Joke-text">{text}</div>
+//     </div>
+//   );
+// }
+
+// Class Component
+class Joke extends React.Component {
+  constructor(props) {
+    super(props)
+    // BIND methods
+    this.upVote = this.upVote.bind(this) 
+    this.downVote = this.downVote.bind(this) 
+  }
+
+  // instance methods 
+  upVote() {
+    this.props.upVote(this.props.id, +1) 
+  }
+  downVote() {
+    this.props.downVote(this.props.id, -1) 
+  }
+
+  render() {
+    // destructure props here 
+    // - upVote, downVote methods
+    // - votes, text
+    const { upVote, downVote, votes, text } = this.props 
+
+    return (
+      <div className="Joke">
+        <div className="Joke-votearea">
+          <button onClick={upVote}>
+            <i className="fas fa-thumbs-up" />
+          </button>
+  
+          <button onClick={downVote}>
+            <i className="fas fa-thumbs-down" />
+          </button>
+  
+          {votes}
+        </div>
+  
+        <div className="Joke-text">{text}</div>
       </div>
-
-      <div className="Joke-text">{text}</div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Joke;
